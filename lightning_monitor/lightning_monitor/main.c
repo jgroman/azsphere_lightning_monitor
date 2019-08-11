45279c8c13d76abfe086b435c3272435f6aa2068
jgroman <jgroman@users.noreply.github.com>
2019-08-11 18:41:35 +0200

    Initial files import

---
+++ lightning_monitor/lightning_monitor/main.c
@@ -0,0 +1,327 @@
+﻿/**
+ * Copyright (c) 2019 Jaroslav Groman
+ *
+ * Licensed under the MIT License.
+ */
+
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+#include <time.h>
+#include <signal.h>
+#include <stdio.h>
+
+#include <applibs/log.h>
+#include <applibs/gpio.h>
+
+// Import project hardware abstraction from project property 
+// "Target Hardware Definition Directory"
+#include <hw/project_hardware.h>
+
+// Using a single-thread event loop pattern based on Epoll and timerfd
+#include "epoll_timerfd_utilities.h"
+
+// RTCore app connection
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <applibs/application.h>
+
+/*******************************************************************************
+* #defines adn constants
+*******************************************************************************/
+
+// !!! Define partner RTCore app Component ID here and also in 
+// Project Properties - Debugging - Partner Components
+#define PARTNER_RTCORE_COMPONENT_ID "ac067706-9bda-425a-a766-a91a2c4e32c0"
+
+
+/*******************************************************************************
+* Forward declarations of private functions
+*******************************************************************************/
+
+/** @brief Application termination handler.
+ *
+ * Signal handler for termination requests. This handler must be
+ * async-signal-safe.
+ *
+ * @param signal_number
+ *
+ */
+static void
+termination_handler(int signal_number);
+
+/** @brief Timer event handler for polling ADC
+ *
+ */
+static void
+adc_timer_event_handler(EventData *event_data);
+
+/** @brief Handle socket event by reading incoming data from real-time capable application.
+ *
+ */
+static void
+socket_event_handler(EventData *eventData);
+
+/** @brief Sends message to real-time capable application.
+ *
+ */
+static void
+send_msg_to_rtcore(void);
+
+/** @brief Initialize signal handlers.
+ *
+ * Set up SIGTERM termination handler.
+ *
+ * @return 0 on success, -1 otherwise.
+ */
+static int
+init_handlers(void);
+
+/** @brief Initialize peripherals.
+ *
+ * Initialize all peripherals used by this project.
+ *
+ * @return 0 on success, -1 otherwise.
+ */
+static int
+init_peripherals(void);
+
+/**
+ *
+ */
+static void
+close_peripherals_and_handlers(void);
+
+/*******************************************************************************
+* Global variables
+*******************************************************************************/
+
+// Termination state flag
+static volatile sig_atomic_t gb_is_termination_requested = false;
+
+static int epoll_fd = -1;
+static int sock_fd = -1;
+
+static int adc_poll_timer_fd = -1;
+static EventData adc_event_data = {          // ADC Event handler data
+    .eventHandler = &adc_timer_event_handler
+};
+
+static EventData socket_event_data = {      // Socket Event handler data
+    .eventHandler = &socket_event_handler
+};
+
+/*******************************************************************************
+* Main program entry
+*******************************************************************************/
+int 
+main(void)
+{
+    Log_Debug("*** App starting\n");
+
+    // Initialize handlers
+    if (init_handlers() != 0)
+    {
+        gb_is_termination_requested = true;
+    }
+
+    // Initialize peripherals
+    if (!gb_is_termination_requested)
+    {
+        if (init_peripherals() != 0)
+        {
+            gb_is_termination_requested = true;
+        }
+    }
+
+    // Main program
+    if (!gb_is_termination_requested)
+    {
+        Log_Debug("Waiting for event\n");
+
+        // Main program loop
+        while (!gb_is_termination_requested)
+        {
+            // Handle timers
+            if (WaitForEventAndCallHandler(epoll_fd) != 0)
+            {
+                gb_is_termination_requested = true;
+            }
+        }
+        Log_Debug("Not waiting for event anymore\n");
+    }
+
+    // Clean up and shutdown
+    close_peripherals_and_handlers();
+}
+
+/*******************************************************************************
+* Private functions
+*******************************************************************************/
+static void
+adc_timer_event_handler(EventData *event_data)
+{
+    // Consume timer event
+    if (ConsumeTimerFdEvent(adc_poll_timer_fd) != 0) {
+        gb_is_termination_requested = true;
+        return;
+    }
+
+    // Request ADC data
+    Log_Debug("ADC check\n");
+
+    send_msg_to_rtcore();
+}
+
+static void 
+send_msg_to_rtcore(void)
+{
+    static int iter = 0;
+
+    // Send "Read-ADC-%d" message to real-time capable application.
+    static char txMessage[32];
+    sprintf(txMessage, "Read-ADC-%d", iter++);
+    Log_Debug("Sending: %s\n", txMessage);
+
+    int bytesSent = send(sock_fd, txMessage, strlen(txMessage), 0);
+    if (bytesSent == -1)
+    {
+        Log_Debug("ERROR: Unable to send message: %d (%s)\n", 
+            errno, strerror(errno));
+        gb_is_termination_requested = true;
+        return;
+    }
+}
+
+static void 
+socket_event_handler(EventData *eventData)
+{
+    // Read response from real-time capable application.
+    char rxBuf[32];
+    union Analog_data
+    {
+        uint32_t u32;
+        uint8_t u8[4];
+    } analog_data;
+
+    int bytesReceived = recv(sock_fd, rxBuf, sizeof(rxBuf), 0);
+
+    if (bytesReceived == -1) 
+    {
+        Log_Debug("ERROR: Unable to receive message: %d (%s)\n", 
+            errno, strerror(errno));
+        gb_is_termination_requested = true;
+    }
+
+    Log_Debug("Received %d bytes.\n", bytesReceived);
+
+    // Copy data from Rx buffer to analog_data union
+    for (int i = 0; i < sizeof(analog_data); i++)
+    {
+        analog_data.u8[i] = rxBuf[i];
+    }
+
+    // Voltage in Volts = 2.5 * adc_reading / 4096
+    double voltage = (2.5 * analog_data.u32) / 4096;
+
+    Log_Debug("Measured voltage: %f V\n", voltage);
+}
+
+static void
+termination_handler(int signal_number)
+{
+    gb_is_termination_requested = true;
+}
+
+static int
+init_handlers(void)
+{
+    int result = -1;
+
+    // Termination handler
+    struct sigaction action;
+    memset(&action, 0, sizeof(struct sigaction));
+    action.sa_handler = termination_handler;
+    result = sigaction(SIGTERM, &action, NULL);
+    if (result != 0) 
+    {
+        Log_Debug("ERROR: %s - sigaction: errno=%d (%s)\n",
+            __FUNCTION__, errno, strerror(errno));
+    }
+
+    // Epoll handler
+    if (result != -1)
+    {
+        epoll_fd = CreateEpollFd();
+        if (epoll_fd < 0) {
+            result = -1;
+        }
+    }
+
+    // Open connection to real-time capable application.
+    sock_fd = Application_Socket(PARTNER_RTCORE_COMPONENT_ID);
+    if (sock_fd == -1)
+    {
+        Log_Debug("ERROR: Unable to create socket: %d (%s)\n", errno, strerror(errno));
+        Log_Debug("Real Time Core disabled or Component Id is not correct.\n");
+        result = -1;
+    }
+    else
+    {
+        // Set timeout, to handle case where real-time capable application 
+        // does not respond.
+        static const struct timeval recvTimeout = { .tv_sec = 5,.tv_usec = 0 };
+        result = setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, 
+                    &recvTimeout, sizeof(recvTimeout));
+        if (result == -1)
+        {
+            Log_Debug("ERROR: Unable to set socket timeout: %d (%s)\n", 
+                errno, strerror(errno));
+        }
+    }
+
+    // Register handler for incoming messages from RTCore
+    if (result != -1)
+    {
+        if (RegisterEventHandlerToEpoll(epoll_fd, sock_fd, 
+                &socket_event_data, EPOLLIN) != 0)
+        {
+            Log_Debug("ERROR: Cannot register receive event handler: %d (%s)\n",
+                errno, strerror(errno));
+            result = -1;
+        }
+    }
+
+    // Create timer for ADC check
+    if (result != -1)
+    {
+        static const struct timespec adc_check_period = { 4, 0 };
+        adc_poll_timer_fd = CreateTimerFdAndAddToEpoll(epoll_fd,
+            &adc_check_period, &adc_event_data, EPOLLIN);
+        if (adc_poll_timer_fd < 0)
+        {
+            Log_Debug("ERROR: Could not create ADC poll timer: %s (%d).\n",
+                strerror(errno), errno);
+            result = -1;
+        }
+    }
+
+    return result;
+}
+
+static int
+init_peripherals(void)
+{
+    int result = 0;
+
+    return result;
+}
+
+static void
+close_peripherals_and_handlers(void)
+{
+    // Close Epoll fd
+    CloseFdAndPrintError(epoll_fd, "Epoll");
+}
+
+/* [] END OF FILE */
---
+++ rtcore_lightning_monitor/main.c
@@ -0,0 +1,262 @@
+/* 
+ * Original work Copyright (c) 2019 Microsoft Corporation. All rights reserved.
+ * Modified work Copyright (c) 2019 Jaroslav Groman
+ *
+ * Licensed under the MIT License. 
+ */
+
+#include <ctype.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <errno.h>
+
+#include "mt3620-baremetal.h"
+#include "mt3620-intercore.h"
+#include "mt3620-uart-poll.h"
+#include "mt3620-adc.h"
+
+// Import project hardware abstraction
+#include "../hardware/avnet_mt3620_sk/inc/hw/project_hardware.h"
+
+#define APP_NAME "RTCore Lightning Monitor"
+
+#define GPIO_USER_LED_RED       8
+#define GPIO_USER_LED_GREEN     9
+#define GPIO_USER_LED_BLUE      10
+#define GPIO_BUTTON_A   12
+
+
+// String CR + LF.
+#define STRING_CRLF  "\r\n"
+ 
+// Uncomment this directive to enable UART debug messages output
+#define DEBUG_ENABLED
+
+/*******************************************************************************
+* Forward declarations of private functions
+*******************************************************************************/
+
+/** @brief Empty exception handler.
+ *
+ */
+static _Noreturn void
+default_exception_handler(void);
+
+/** @brief Main application.
+ *
+ */
+static _Noreturn void
+rtcore_main(void);
+
+/** @brief Print bytes from buffer.
+ *
+ */
+static void
+print_bytes(const uint8_t *buf, int start, int end);
+
+static void 
+print_guid(const uint8_t *guid);
+
+/*******************************************************************************
+* Global variables
+*******************************************************************************/
+
+extern uint32_t StackTop; // &StackTop == end of TCM0
+
+/*
+ARM DDI0403E.d SB1.5.2-3
+From SB1.5.3, "The Vector table must be naturally aligned to a power of two
+whose alignment value is greater than or equal to (Number of Exceptions 
+supported x 4), with a minimum alignment of 128 bytes.". The array is aligned 
+in linker.ld, using the dedicated section ".vector_table".
+*/
+
+// The exception vector table contains a stack pointer, 15 exception handlers, 
+// and an entry for each interrupt.
+#define INTERRUPT_COUNT 100 // from datasheet
+#define EXCEPTION_COUNT (16 + INTERRUPT_COUNT)
+#define INT_TO_EXC(i_) (16 + (i_))
+static const uintptr_t ExceptionVectorTable[EXCEPTION_COUNT]
+    __attribute__((section(".vector_table"))) __attribute__((used)) = {
+        [0] = (uintptr_t)&StackTop,                  // Main Stack Pointer (MSP)
+        [1] = (uintptr_t)rtcore_main,                // Reset
+        [2] = (uintptr_t)default_exception_handler,  // NMI
+        [3] = (uintptr_t)default_exception_handler,  // HardFault
+        [4] = (uintptr_t)default_exception_handler,  // MPU Fault
+        [5] = (uintptr_t)default_exception_handler,  // Bus Fault
+        [6] = (uintptr_t)default_exception_handler,  // Usage Fault
+        [11] = (uintptr_t)default_exception_handler, // SVCall
+        [12] = (uintptr_t)default_exception_handler, // Debug monitor
+        [14] = (uintptr_t)default_exception_handler, // PendSV
+        [15] = (uintptr_t)default_exception_handler, // SysTick
+
+        [INT_TO_EXC(0)... INT_TO_EXC(INTERRUPT_COUNT - 1)] = 
+            (uintptr_t)default_exception_handler };
+
+/*******************************************************************************
+* Application entry point
+*******************************************************************************/
+
+static _Noreturn void 
+rtcore_main(void)
+{
+	union Analog_data
+	{
+		uint32_t u32;
+		uint8_t u8[4];
+	} analog_data;
+
+    // SCB->VTOR = ExceptionVectorTable
+    WriteReg32(SCB_BASE, 0x08, (uint32_t)ExceptionVectorTable);
+
+#   ifdef DEBUG_ENABLED
+    // UART initialization and app header
+    Uart_Init();
+    Uart_WriteStringPoll(APP_NAME " ("__DATE__ ", " __TIME__") READY" STRING_CRLF);
+#   endif // DEBUG_ENABLED
+
+	// ADC Initialization
+	EnableAdc();
+
+    BufferHeader *outbound, *inbound;
+    uint32_t sharedBufSize = 0;
+    while (GetIntercoreBuffers(&outbound, &inbound, &sharedBufSize) == -1)
+    {
+        // Empty block, waiting for buffers to become available
+    }
+
+    // Main program loop
+#   define PAYLOAD_START_IDX   20
+#   define MAX_APP_BUFFER_SIZE 256
+    uint8_t app_buf[MAX_APP_BUFFER_SIZE];
+    uint32_t read_bytes_count = sizeof(app_buf);
+    for (;;)
+	{
+        // Read incoming data from A7 Core
+        // On success, read_bytes_count is set to the number of read bytes.
+        int dequeue_result = DequeueData(outbound, inbound, sharedBufSize, 
+            app_buf, &read_bytes_count);
+
+        if (dequeue_result == -1 || read_bytes_count < PAYLOAD_START_IDX)
+		{
+            continue;
+        }
+
+#       ifdef DEBUG_ENABLED
+        // Print incoming message header
+        Uart_WriteStringPoll("Received message of ");
+        Uart_WriteIntegerPoll(read_bytes_count);
+        Uart_WriteStringPoll("bytes:" STRING_CRLF);
+
+        // Print the Component Id (A7 Core)
+        Uart_WriteStringPoll("  Component Id (16 bytes): ");
+        print_guid(app_buf);
+        Uart_WriteStringPoll(STRING_CRLF);
+
+        // Print reserved field as little-endian 4-byte integer.
+        Uart_WriteStringPoll("  Reserved (4 bytes): ");
+        print_bytes(app_buf, 19, 16);
+        Uart_WriteStringPoll(STRING_CRLF);
+
+        // Print message as hex.
+        size_t payloadBytes = read_bytes_count - PAYLOAD_START_IDX;
+        Uart_WriteStringPoll("  Payload (");
+        Uart_WriteIntegerPoll(payloadBytes);
+        Uart_WriteStringPoll(" bytes as hex): ");
+
+        for (size_t i = PAYLOAD_START_IDX; i < read_bytes_count; ++i)
+        {
+            Uart_WriteHexBytePoll(app_buf[i]);
+            if (i != read_bytes_count - 1)
+            {
+                Uart_WriteStringPoll(":");
+            }
+        }
+        Uart_WriteStringPoll(STRING_CRLF);
+
+        // Print message as text.
+        Uart_WriteStringPoll("  Payload (");
+        Uart_WriteIntegerPoll(payloadBytes);
+        Uart_WriteStringPoll(" bytes as text): ");
+        char printed_char_str[2];
+        printed_char_str[1] = '\0';
+        for (size_t i = PAYLOAD_START_IDX; i < read_bytes_count; ++i)
+        {
+            printed_char_str[0] = isprint(app_buf[i]) ? app_buf[i] : '.';
+            Uart_WriteStringPoll(printed_char_str);
+        }
+        Uart_WriteStringPoll(STRING_CRLF);
+#       endif // DEBUG_ENABLED
+
+        // Read ADC channel
+        uint8_t adc_channel = 1;
+        analog_data.u32 = ReadAdc(adc_channel);
+
+#ifdef DEBUG_ENABLED
+        uint32_t mV;
+        mV = (analog_data.u32 * 2500) / 0xFFF;
+        Uart_WriteStringPoll("ADC channel ");
+        Uart_WriteIntegerPoll(adc_channel);
+        Uart_WriteStringPoll(" : ");
+        Uart_WriteIntegerPoll(mV / 1000);
+        Uart_WriteStringPoll(".");
+        Uart_WriteIntegerWidthPoll(mV % 1000, 3);
+        Uart_WriteStringPoll(" V");
+        Uart_WriteStringPoll(STRING_CRLF);
+#endif // DEBUG_ENABLED
+
+        uint8_t analog_buf_idx = 0;
+		for (int buf_idx = 0; buf_idx < 4; buf_idx++)
+		{
+			// Copy ADC data to app buffer
+            app_buf[PAYLOAD_START_IDX + buf_idx] = 
+                analog_data.u8[analog_buf_idx++];
+		}
+
+		// Send buffer to A7 Core
+        EnqueueData(inbound, outbound, sharedBufSize, app_buf, 
+            PAYLOAD_START_IDX + 4);
+    }
+}
+
+/*******************************************************************************
+* Private functions
+*******************************************************************************/
+
+static _Noreturn void
+default_exception_handler(void)
+{
+    for (;;)
+    {
+        // Empty Block
+    }
+}
+
+static void
+print_bytes(const uint8_t *buf, int start, int end)
+{
+    int step = (end >= start) ? +1 : -1;
+
+    for (/* nop */; start != end; start += step)
+    {
+        Uart_WriteHexBytePoll(buf[start]);
+    }
+    Uart_WriteHexBytePoll(buf[end]);
+}
+
+static void
+print_guid(const uint8_t *guid)
+{
+    print_bytes(guid, 3, 0); // 4-byte little-endian word
+    Uart_WriteStringPoll("-");
+    print_bytes(guid, 5, 4); // 2-byte little-endian half
+    Uart_WriteStringPoll("-");
+    print_bytes(guid, 7, 6); // 2-byte little-endian half
+    Uart_WriteStringPoll("-");
+    print_bytes(guid, 8, 9); // 2 bytes
+    Uart_WriteStringPoll("-");
+    print_bytes(guid, 10, 15); // 6 bytes
+}
+
+/* [] END OF FILE */
